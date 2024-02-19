78c345c04e45d4714c748756a977c5f4d0ee4d18
Thien <nthien.se@gmail.com>
2024-02-19 13:11:16 +0700

    add submission

--- src/submission/pages/ViewPage/index.tsx
+++
@@ -1,9 +0,0 @@
-const ViewPage = () => {
-  return (
-    <div>
-      <h1>Submisionn View Page</h1>
-    </div>
-  );
-};
-
-export default ViewPage;
--- src/submission/routes/PublicRoute.tsx
+++ src/submission/routes/PublicRoute.tsx
@@ -1,29 +1,19 @@
 import { lazy } from "react";
-import { Navigate } from "react-router-dom";
 import { SUBMISSION_URL } from "submission/constants";
 
 import { RouteObject } from "react-router-dom";
 
 const MainContainer = lazy(() => import("submission/containers/MainContainer"));
-const ListPage = lazy(() => import("submission/pages/ListPage"));
-const ViewPage = lazy(() => import("submission/pages/ViewPage"));
+const MainPage = lazy(() => import("submission/pages/MainPage"));
 
 const routes: RouteObject = {
   path: SUBMISSION_URL,
   element: <MainContainer />,
   children: [
     {
-      index: true,
-      element: <Navigate to={`/${SUBMISSION_URL}/list`} />,
-    },
-    {
-      path: "list/*",
-      element: <ListPage />,
-    },
-    {
-      path: "view/:id",
-      element: <ViewPage />,
-    },
+      path: "",
+      element: <MainPage />,
+    }
   ],
 };
 
